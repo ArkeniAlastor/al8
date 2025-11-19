97dae43b4c59d48dc308eb39707da88fc17b42ac
zuboda48 <zuboda48@29-3>
2025-11-19 10:19:20 +0200

    mhb

--- al7/al7.cpp
+++ al7/al7.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() 
 {
+	srand(time(0));
+	
+	vector<int> vec{ 1,2,3,4,5 };
+
+	for ( auto ptr = vec.begin(); ptr != vec.end(); ptr++)
+	{
+		cout << *ptr << "\t";
+	}
+
+	cout << endl;
+
+	vec.push_back(99);
+	vec.push_back(88);
+	vec.push_back(77);
+	vec.push_back(66);
+	vec.push_back(55);
+	vec.push_back(44);
+
+	vec.insert(vec.begin(), 200);
+	vec.insert(vec.begin(), 211);
+	vec.insert(vec.begin(), 222);
+	vec.insert(vec.begin(), 233);
+	vec.insert(vec.begin(), 244);
+
+	for (auto ptr = vec.begin(); ptr != vec.end(); ptr++)
+	{
+		cout << *ptr << "\t";
+	}
+
+	cout << endl;
 
 }
