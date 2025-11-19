64012ac5efd234130e41233e06295495ccb5d1c4
nameArkeniAlastorgit config --global user.email <parkerosnova@gmail.com>
2025-11-19 22:51:24 +0200

    ff

--- al7/al7.cpp
+++ al7/al7.cpp
@@ -1,10 +1,53 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 using namespace std;
 
 int main() 
 {
-	srand(time(0));
+
+	/*дз.
+Создать вектор для вещественных чисел.
+Добавить 5 элементов в конец
+пoKa3aTb BeKTор
+добавить еще 10 элементов в начало
+показать вектор!*/
+
+	vector<double> vec{-6.6, -7.7, -8.8, -9.9, -10.10};
+	vector<double> vect{-10.10, -20.20, -30.30, -40.40, -50.50, -60.60, -70.70, -80.80, -90.90, -100.100 };
+
+	vec.push_back(1.1);
+	vec.push_back(2.2);
+	vec.push_back(3.3);
+	vec.push_back(4.4);
+	vec.push_back(5.5);
+
+	vect.insert(vect.begin(), 10.10);
+	vect.insert(vect.begin(), 20.20);
+	vect.insert(vect.begin(), 30.30);
+	vect.insert(vect.begin(), 40.40);
+	vect.insert(vect.begin(), 50.50);
+	vect.insert(vect.begin(), 60.60);
+	vect.insert(vect.begin(), 70.70);
+	vect.insert(vect.begin(), 80.80);
+	vect.insert(vect.begin(), 90.90);
+	vect.insert(vect.begin(), 100.100);
+
+
+
+	for (auto ptr = vec.begin(); ptr != vec.end(); ptr++ )
+	{
+		cout << *ptr << "\t";
+	}
+	cout << endl;
+
+	for (auto ptr = vect.begin(); ptr != vect.end(); ptr++)
+	{
+		cout << *ptr << "\t";
+	}
+	cout << endl;	
+	
+
+	/*srand(time(0));
 	
 	vector<int> vec{ 1,2,3,4,5 };
 
@@ -33,6 +76,7 @@ int main()
 		cout << *ptr << "\t";
 	}
 
-	cout << endl;
+	cout << endl;*/
 
 }
+
